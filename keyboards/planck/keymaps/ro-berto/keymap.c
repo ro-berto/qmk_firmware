5bab8c29817f72a78738eb76a48f809ca3656260
Roberto Carrillo <robertocn@google.com>
2018-07-23 14:59:01 -0700

    Make ag_swap only affect left alt/gui

--- keyboards/planck/keymaps/ro-berto/keymap.c
+++ keyboards/planck/keymaps/ro-berto/keymap.c
@@ -42,8 +42,14 @@ enum planck_keycodes {
 #define RAISE MO(_RAISE)
 
 /* Roberto's customizations */
+/* Make fn key work as caps lock when tapping, altgr when holding */
 #define MY_CAPS ALGR_T(KC_CAPSLOCK)
+/* Make right shift be enter on tap, shift on hold */
 #define MY_ENT MT(MOD_RSFT, KC_ENT)
+
+/* Make alt/gui swap only affect left alt, to keep altgr in bottom left */
+#define MY_AG_NORM MAGIC_UNSWAP_LALT_LGUI
+#define MY_AG_SWAP MAGIC_SWAP_LALT_LGUI
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -168,10 +174,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = {
-  {_______, RESET,   DEBUG,    RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_DEL },
-  {_______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK, DVORAK,  PLOVER,  _______},
-  {_______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
+  {_______, RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI,    RGB_HUD,    RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, KC_DEL },
+  {_______, _______, MU_MOD,  AU_ON,   AU_OFF,  MY_AG_NORM, MY_AG_SWAP, QWERTY,  COLEMAK,  DVORAK,  PLOVER,  _______},
+  {_______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,      MI_OFF,     TERM_ON, TERM_OFF, _______, _______, _______},
+  {_______, _______, _______, _______, _______, _______,     _______,   _______, _______,  _______, _______, _______}
 }
 
 
