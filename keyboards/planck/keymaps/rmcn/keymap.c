b6c01e3848b211f95f562af2bc72aefbc1b45d46
Roberto Carrillo <robertocn@google.com>
2019-08-29 12:23:33 -0700

    Group colors, remove unused Non-US keys

--- keyboards/planck/keymaps/rmcn/keymap.c
+++ keyboards/planck/keymaps/rmcn/keymap.c
@@ -133,16 +133,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  |   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO | | Home | End  |      |
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |      | Home | End  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_grid(
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______,
-    _______, _______, _______, _______, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
+    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
+    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, KC_HOME, KC_END,  _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
 /* Raise
@@ -151,7 +151,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |Pg Up |Pg Dn |      |
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |      |Pg Up |Pg Dn |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
@@ -159,7 +159,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT_planck_grid(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
+    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, KC_PGUP, KC_PGDN, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 /* Macro
@@ -348,62 +348,69 @@ void color_cp(uint32_t in, RGB *out) {
   out->b = in & 255;
 }
 
+
 void classify(uint16_t code, RGB *res){
   switch(code) {
+ // OFF
     case KC_SPC:
     case XXXXXXX:
       color_cp(0x000000, res);
       break;
+ // LETTERS (Caps)
     case KC_A ... KC_Z:
       color_cp(letter_color, res);
       break;
+    case KC_CAPSLOCK:
+      color_cp(0xFF0000, res);
+      break;
+ // NUMBERS
+    case RAISE:
     case KC_1 ... KC_0:
-      color_cp(0x000099, res);
-      break;
-    case KC_UNDS ... KC_QUES:
-    case KC_MINUS ... KC_SLASH:
-      color_cp(0x999900, res);
-      break;
-    case KC_F1...KC_F12:
-      color_cp(0x999999, res);
-      break;
-    case KC_INSERT...KC_UP:
-      color_cp(0x449944, res);
-      break;
     case NPKC:
     case KC_KP_SLASH...KC_KP_DOT:
       color_cp(0x0000FF, res);
       break;
+ // SYMBOLS
+    case KC_EXLM ... KC_RPRN:
+    case LOWER:
+    case KC_UNDS ... KC_QUES:
+    case KC_MINUS ... KC_SLASH:
+      color_cp(0x999900, res);
+      break;
+ // FKEYS
+    case KC_F1...KC_F12:
+      color_cp(0xFFFFFF, res);
+      break;
+ // MOVEMENT (Home/End/PgUp/PgDown/Arrows)
     case KC_BSPACE:
-      color_cp(0x770000, res);
+    case KC_INSERT...KC_UP:
+      color_cp(0x44FF44, res);
       break;
-    case KC_CAPSLOCK:
-      color_cp(0x880044, res);
-      break;
+ // MEDIA (Vol, next, play, beep on, beep off)
     case KC__MUTE...KC__VOLDOWN:
     case KC_AUDIO_MUTE...KC_MEDIA_REWIND:
       color_cp(0xFF3366, res);
       break;
+ // MODIFIERS
     case KC_LCTRL...KC_RGUI:
       color_cp(0xFFFF99, res);
       break;
+ // EMOJI MACRO
     case FIRST_EMOJI...LAST_EMOJI:
       color_cp(0xFFCC00, res);
       break;
+ // OTHER MACRO
     case FIRST_SPANISH...LAST_SPANISH:
-      color_cp(0xFF0000, res);
-      break;
-    case RAISE:
-      color_cp(0x000099, res);
-      break;
-    case RESET:
-      color_cp(0xFF0000, res);
-      break;
     case MR_COMB:
       color_cp(0xFF00FF, res);
       break;
+ // SPECIAL (RESET)
+    case RESET:
+      color_cp(0xFF0000, res);
+      break;
+ // DEFAULT
     default:
-      color_cp(0x994400, res);
+      color_cp(0x99FF00, res);
       break;
   }
 }
